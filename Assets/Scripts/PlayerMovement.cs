f35b737479cfb9d346897a6e2ca07aac59c522dc
juankarf <jcrfaudiovisual@hotmail.com>
2022-11-08 19:41:04 +0100

    Subir Proyecto

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator anim;
+    public float speed = 10;
+    public float rotationSpeed = 10;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       float vertical = Input.GetAxis("Vertical");
+       if(vertical>0)
+        {
+            rb.AddForce(transform.up * vertical * speed * Time.deltaTime);
+            anim.SetBool("Impulsing", true);
+        }
+       else
+        {
+            anim.SetBool("Impulsing", false);
+        }
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.eulerAngles = transform.eulerAngles + new Vector3(0, 0, horizontal * rotationSpeed * Time.deltaTime);
+
+
+    }
+}
